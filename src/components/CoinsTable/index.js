e63f167d208fb5c01c6033e9bbe458a78a5afc59
Tittoh <tittohb@gmail.com>
2019-03-06 23:39:02 +0300

    Fetch top 10 coins

--- src/components/CoinsTable/index.js
+++ src/components/CoinsTable/index.js
@@ -1,39 +1,16 @@
 import React from 'react';
-import { Table, Card } from 'react-materialize'
 
-class CoinsTable extends React.Component {
-  render() {
-    return (
-      <Card className="max-width">
-      <Table className="bordered">
-        <thead>
-          <tr>
-            <th data-field="id">Name</th>
-            <th data-field="name">Item Name</th>
-            <th data-field="price">Item Price</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td>Alvin</td>
-            <td>Eclair</td>
-            <td>$0.87</td>
-          </tr>
-          <tr>
-            <td>Alan</td>
-            <td>Jellybean</td>
-            <td>$3.76</td>
-          </tr>
-          <tr>
-            <td>Jonathan</td>
-            <td>Lollipop</td>
-            <td>$7.00</td>
-          </tr>
-        </tbody>
-      </Table>
-      </Card>
-    )
-  }
-}
+const CoinsTable = ({ coin }) =>{
+  const imgLink = `https://www.cryptocompare.com${coin.CoinInfo.ImageUrl}`
+  return (
+    <li className="collection-item avatar">
+      <img src={imgLink} alt="icon" className="circle" />
+      <span className="title">{coin.CoinInfo.FullName}</span>
+      <p>{coin.CoinInfo.Name}
+        {coin.RAW.USD.PRICE}
+      </p>
+      <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
+    </li>
+  )
+};
 export default CoinsTable;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import { Navbar, NavItem, Icon } from 'react-materialize'
 class Nav extends React.Component {
   render() {
     return (
-      <Navbar brand='logo' right>
+      <Navbar brand='Crypto Market' right className="blue-grey darken-4">
         <NavItem href='#'><Icon>search</Icon></NavItem>
         <NavItem href='#'><Icon>view_module</Icon></NavItem>
         <NavItem href='#'><Icon>refresh</Icon></NavItem>
